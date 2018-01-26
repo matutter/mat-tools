21486084813d3412fd4020bd0ae06644fbd4d4bf
matutter <matutter4@gmail.com>
2018-01-26 16:13:42 -0500

    renamed aliases

--- aliases.sh
+++ aliases.sh
@@ -3,8 +3,8 @@
 # Aliases for scripts in `scripts/` are prefixed with mt and copied to `~/.bin`.
 #
 
-alias mt-env-activate='. ~/.bin/env-activate.sh'
+alias virtualenc-enable='. ~/.bin/env-activate.sh'
 
-alias mt-ssh-agent='~/.bin/ssh-agent.sh'
+alias ssh-agent-enable='~/.bin/ssh-agent.sh'
 
-alias mt-monitor='~/.bin/monitor.sh'
+alias dev-watch='~/.bin/monitor.sh'
