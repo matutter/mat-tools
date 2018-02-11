8bdfe5a299227063e88792a03c49c6889ba650b9
matutter <matutter4@gmail.com>
2018-02-10 22:32:49 -0500

    fixed typo in aliases

--- aliases.sh
+++ aliases.sh
@@ -3,7 +3,7 @@
 # Aliases for scripts in `scripts/` are prefixed with mt and copied to `~/.bin`.
 #
 
-alias virtualenc-enable='. ~/.bin/env-activate.sh'
+alias virtualenv-enable='. ~/.bin/env-activate.sh'
 
 alias ssh-agent-enable='~/.bin/ssh-agent.sh'
 
